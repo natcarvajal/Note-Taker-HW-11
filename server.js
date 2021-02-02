b5efab7260871100f7fae4a93c9c578909d54c31
Natalia Carvajal <nataliacarvajal1996@gmail.com>
2021-02-02 13:44:42 -0500

    push

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 // dependencies
+const fs = require("fs");
 const express = require("express");
 var path = require("path");
 
 const app = express();
 
 // routes
-app.get("/", function (req, res) {
+app.get("*", function (req, res) {
   res.sendFile(__dirname + "/public/index.html");
 });
 
@@ -13,7 +14,19 @@ app.get("/notes", function (req, res) {
   res.sendFile(__dirname + "/public/notes.html");
 });
 
+app.post()
+
 // listening
 app.listen(8080, function () {
   console.log("Listening on port 8080");
 });
+
+// The application should have a db.json file on the backend that will be used to store and retrieve notes using the fs module.
+
+// The following API routes should be created:
+
+// GET /api/notes - Should read the db.json file and return all saved notes as JSON.
+
+// POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+
+// DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
