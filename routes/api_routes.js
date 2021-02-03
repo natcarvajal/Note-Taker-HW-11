808f80ef1eb00285d6e0ac869c0df586e456db3d
Natalia Carvajal <nataliacarvajal1996@gmail.com>
2021-02-03 01:30:45 -0500

    post in progress

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,9 +1,8 @@
-const { response } = require("express");
 const fs = require("fs");
 const path = require("path");
-const db = __dirname + "/../db/db.json";
+const db = require("../db/db.json");
 const { v4: uuidv4 } = require("uuid");
-
+// console.log("db: ", db);
 module.exports = (app) => {
   app.get("/api/notes", (req, res) => {
     fs.readFile(db, "utf-8", (err, data) => {
@@ -20,30 +19,29 @@ module.exports = (app) => {
     // Rewrite the file with the new array
     // Respond with the new array
     fs.readFile(db, "utf-8", (err, data) => {
-      let notes = [];
       if (err) throw err;
       const newNote = req.body;
       newNote.id = uuidv4();
-      notes.push(newNote);
-      fs.writeFile(
-        path.join(__dirname + "/../db/db.json", JSON.stringify(data))
-      );
-      response.json(data);
+      db.push(newNote);
+      fs.writeFile(__dirname, "/../db/db.json"),
+        JSON.stringify(newNote),
+        "utf-8";
+      res.json(data);
     });
   });
 
-  app.delete("/api/notes/:id", (req, res) => {
-    let del = req.params.id;
-    fs.readFile(db, "utf-8", (err, data) => {
-      if (err) throw err;
-      let notes = JSON.parse(data);
-      db = db.filter((notes) => notes.id != del);
-      response.json(notes);
-    });
-    // // I can get access to the id through req.params.id
-    // Read the file
-    // Find the UUID matching the one in the params object
-    // Delete it
-    // Send back the array with the deleted note
-  });
+  //   app.delete("/api/notes/:id", (req, res) => {
+  //     let del = req.params.id;
+  //     fs.readFile(db, "utf-8", (err, data) => {
+  //       if (err) throw err;
+  //       let notes = JSON.parse(data);
+  //       db = db.filter((notes) => notes.id != del);
+  //       res.json(notes);
+  //     });
+  // // I can get access to the id through req.params.id
+  // Read the file
+  // Find the UUID matching the one in the params object
+  // Delete it
+  // Send back the array with the deleted note
+  //   });
 };
